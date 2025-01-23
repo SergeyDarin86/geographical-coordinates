95aba4d118e4a69e7accf31de715212150302779
sergei_darin <swd86@mail.ru>
2025-01-23 16:33:34 +0400

    Добавил логгирование в проект

--- src/main/java/ru/darin/coordinates/controller/CoordinatesController.java
+++ src/main/java/ru/darin/coordinates/controller/CoordinatesController.java
@@ -1,6 +1,7 @@
 package ru.darin.coordinates.controller;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -14,22 +15,26 @@ import ru.darin.coordinates.util.exceptions.CoordinatesException;
 import ru.darin.coordinates.util.exceptions.ExceptionBuilder;
 import ru.darin.coordinates.util.exceptions.CoordinatesExceptionNotFound;
 
+@Slf4j
 @RestController
 public class CoordinatesController {
 
     //TODO:
     // + 1)  добавить БД в проект (возможно будет одна таблица)
     // 2) сделать DTO + Mapper
-    // 3) сделать валидацию вводимых данных (только строки)
+    // +3) сделать валидацию вводимых данных (только строки)
     // !
-    // 3.1 - сделать свой кастомный валидатор, т.к. если в поле региона ничего не ввести будет две ошибки - NotEmpty() и Pattern()
+    // +3.1 - сделать свой кастомный валидатор, т.к. если в поле региона ничего не ввести будет две ошибки - NotEmpty() и Pattern()
     // !
-    // 4) добавить ExceptionHandler (такого региона нет; форма для ввода не должна быть пустой)
-    // 4.1) ExceptionHandler для федеральных округов
+    // 4)+ добавить ExceptionHandler (такого региона нет; форма для ввода не должна быть пустой)
+    // 4.1)+ ExceptionHandler для федеральных округов
     // 5) запуск в докере
     // 6) тестирование
     // 7) документация
-    // 8) логгирование
+    // 8)+ логгирование
+    // 9)+ Посмотреть, возможно у меня неправильный подсчет центра для округов -
+    // может там нужно считать количество "кусочков" и для каждого такого куска высчитывать центр
+    // и далее уже общий
     private final CoordinatesService service;
 
     public CoordinatesController(CoordinatesService service) {
@@ -38,6 +43,7 @@ public class CoordinatesController {
 
     @GetMapping("/getGeoCenter")
     public ResponseEntity getGeoCenterForRegion(@RequestBody @Valid SearchDTOForRegion searchDTOForRegion, BindingResult bindingResult){
+        log.info("Start method  getGeoCenterForRegion(searchDTOForRegion) for CoordinatesController, location is: {} ", searchDTOForRegion.getLocation());
         ExceptionBuilder.buildErrorMessageForClient(bindingResult);
         String url = "https://nominatim.openstreetmap.org/search?state=" + searchDTOForRegion.getLocation() + "&country=russia&format=json&polygon_geojson=1";
         ExceptionBuilder.buildErrorMessageForClientRegionNotFound(service.getResponseForRegion(url));
@@ -46,6 +52,7 @@ public class CoordinatesController {
 
     @GetMapping("/getGeoCenterForDistrict")
     public CoordinateResponse getGeoCenterForDistrict(@RequestBody @Valid SearchDTOForDistrict searchDTO, BindingResult bindingResult){
+        log.info("Start method  getGeoCenterForDistrict(searchDTO) for CoordinatesController, location is: {} ", searchDTO.getLocation());
         ExceptionBuilder.buildErrorMessageForClient(bindingResult);
         String url = "https://nominatim.openstreetmap.org/search?state=" + searchDTO.getLocation() + "&country=russia&format=json&polygon_geojson=1";
         ExceptionBuilder.buildErrorMessageForClientDistrictNotFound(service.getResponseForDistrict(url));
@@ -58,15 +65,17 @@ public class CoordinatesController {
                 e.getMessage(),
                 System.currentTimeMillis()
         );
+        log.error("Finish method handlerException() for CoordinatesController, statusCode is: {}, message is: {} ", HttpStatus.BAD_REQUEST, response.getMessage());
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler
-    private ResponseEntity<CoordinatesErrorResponse> libraryHandlerException(CoordinatesExceptionNotFound e) {
+    private ResponseEntity<CoordinatesErrorResponse> coordinatesHandlerException(CoordinatesExceptionNotFound e) {
         CoordinatesErrorResponse response = new CoordinatesErrorResponse(
                 e.getMessage(),
                 System.currentTimeMillis()
         );
+        log.error("Finish method coordinatesHandlerException() for CoordinatesController, statusCode is: {}, message is: {} ", HttpStatus.NOT_FOUND, response.getMessage());
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
--- src/main/java/ru/darin/coordinates/service/CoordinatesService.java
+++ src/main/java/ru/darin/coordinates/service/CoordinatesService.java
@@ -1,5 +1,6 @@
 package ru.darin.coordinates.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +14,7 @@ import ru.darin.coordinates.util.LocationForDistrict;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 public class CoordinatesService {
 
@@ -34,6 +36,7 @@ public class CoordinatesService {
 
     @Cacheable("cacheForRegion")
     public CoordinateResponse getCoordinatesForRegion(String url) {
+        log.info("Start method getCoordinatesForRegion(url) for CoordinatesService, url is: {} ", url);
         Location[] response = getResponseForRegion(url);
         int countOfCoordinates = response[0].getGeoJson().getCoordinates().get(0).size();
         double longitude = 0;
@@ -47,12 +50,14 @@ public class CoordinatesService {
         double centerForLongitude = longitude / countOfCoordinates;
         double centerForLatitude = latitude / countOfCoordinates;
         saveCoordinates(response[0].getName(), centerForLongitude, centerForLatitude);
+        log.info("Finish method getCoordinatesForRegion(url) for CoordinatesService, regionName is: {} ", response[0].getName());
 
         return new CoordinateResponse(response[0].getName(), new CoordinatesForGeoCenter(centerForLatitude, centerForLongitude));
     }
 
     @Cacheable("cacheForDistrict")
     public CoordinateResponse getCoordinatesForDistrict(String url) {
+        log.info("Start method getCoordinatesForDistrict(url) for CoordinatesService, url is: {} ", url);
         double longitude = 0;
         double latitude = 0;
 
@@ -68,6 +73,7 @@ public class CoordinatesService {
         double centerForLongitude = longitude / countOfCoordinates;
         double centerForLatitude = latitude / countOfCoordinates;
         saveCoordinates(response[0].getName(), centerForLongitude, centerForLatitude);
+        log.info("Finish method getCoordinatesForDistrict(url) for CoordinatesService, districtName is: {} ", response[0].getName());
 
         return new CoordinateResponse(response[0].getName(), new CoordinatesForGeoCenter(centerForLatitude, centerForLongitude));
     }
@@ -78,10 +84,12 @@ public class CoordinatesService {
             if (maxPartOfDistrict.size() > floats.get(0).size()) continue;
             maxPartOfDistrict = floats.get(0);
         }
+        log.info("Finish method getMaxPartOfDistrict() for CoordinatesService, size of max part for District is: {} ", maxPartOfDistrict.size());
         return maxPartOfDistrict;
     }
 
     public void saveCoordinates(String regionName, double longitude, double latitude) {
+        log.info("Start method saveCoordinates(regionName, longitude, latitude) for CoordinatesService, regionName is: {}, longitude is: {}, latitude is: {} ", regionName, longitude, latitude);
         Region region = new Region();
         region.setRegionName(regionName);
         region.setLongitude(longitude);
