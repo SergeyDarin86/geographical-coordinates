ab09969486bcaefdb4e32fb7b87c5b7e6ccf4679
sergei_darin <swd86@mail.ru>
2025-01-28 14:48:38 +0400

    Начал тестирование сервиса - метод getCoordinatesForRegion()

--- src/test/java/ru/darin/coordinates/service/CoordinatesServiceTest.java
+++ src/test/java/ru/darin/coordinates/service/CoordinatesServiceTest.java
@@ -8,9 +8,17 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.web.client.RestTemplate;
 import ru.darin.coordinates.model.Region;
 import ru.darin.coordinates.repository.RegionRepository;
+import ru.darin.coordinates.util.CoordinateResponse;
+import ru.darin.coordinates.util.GeoJson;
 import ru.darin.coordinates.util.Location;
 import ru.darin.coordinates.util.LocationForDistrict;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -27,9 +35,9 @@ class CoordinatesServiceTest {
     @InjectMocks
     private CoordinatesService service;
 
-    private String urlForRegion = "url/region";
+    private String urlForRegion = "http://example-region.com";
 
-    private String urlForDistrict = "url/district";
+    private String urlForDistrict = "http://example-district.com";
 
     @BeforeEach
     public void setUp() {
@@ -52,12 +60,31 @@ class CoordinatesServiceTest {
 
     @Test
     void getCoordinatesForRegion() {
+        GeoJson geoJson = new GeoJson("gson");
+        Location location = new Location(111, "licence", "region", geoJson);
 
+        List<List<Float>> coordinates = new ArrayList<>();
+        coordinates.add(Arrays.asList(1.0f, 2.0f));
+        coordinates.add(Arrays.asList(3.0f, 4.0f));
+
+        geoJson.setCoordinates(Collections.singletonList(coordinates));
+        location.setGeoJson(geoJson);
+        location.setName("Тульская область");
+
+        when(service.getResponseForRegion(urlForRegion)).thenReturn(new Location[]{location});
+
+        CoordinateResponse result = service.getCoordinatesForRegion(urlForRegion);
+
+        assertEquals("Тульская область", result.getName());
+        assertEquals(2.0, result.getCoordinatesForGeoCenter().getLongitude(), 0.001);
+        assertEquals(3.0, result.getCoordinatesForGeoCenter().getLatitude(), 0.001);
     }
 
     @Test
     void getCoordinatesForDistrict() {
+
     }
+
 
     @Test
     void getMaxPartOfDistrict() {
